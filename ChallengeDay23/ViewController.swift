0d53836c6b6259a746980cea5e077cfa8c53d8c1
GeorgEvseyev <georgevseyev@icloud.com>
2022-06-03 19:21:43 +0300

    Initial Commit

---
+++ ChallengeDay23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeDay23
+//
+//  Created by Георгий Евсеев on 3.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
