9e16304d5f30c5b9c27f616de86fa5a0d294ef36
GeorgEvseyev <evseyevgeorg@gmail.com>
2023-05-03 21:32:39 +0300

    Update

--- ChallengeDay23/ViewController.swift
+++ ChallengeDay23/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var flags = [String]()
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "The Flag"
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasSuffix("png") {
+                flags.append(item)
+            }
+        }
+        print(flags)
+
+        let sortedFlags = flags.sorted(by: <)
+        flags = sortedFlags
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        cell.imageView?.image = UIImage(named: flags[indexPath.row])
+        
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedFlag = flags[indexPath.row]
+
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
-
